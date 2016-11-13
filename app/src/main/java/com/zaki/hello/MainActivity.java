ce0cc2e3dc481549aedc7ec89a170650fc501a9e
yamayosi <vanha.muist@gmail.com>
2016-11-14 00:54:47 +0900

    AlertDialogにxmlを適応

--- app/src/main/java/com/zaki/hello/MainActivity.java
+++ app/src/main/java/com/zaki/hello/MainActivity.java
@@ -1,18 +1,15 @@
 package com.zaki.hello;
 
 import android.app.Activity;
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
-import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import static android.widget.Toast.LENGTH_SHORT;
-
 public class MainActivity extends Activity {
-
 
 
     @Override
@@ -20,19 +17,35 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final LinearLayout linearLayout = (LinearLayout) findViewById(R.id.activity_main);
 
-        TextView textView = (TextView)findViewById(R.id.text01);
+        TextView textView = (TextView) findViewById(R.id.text01);
         textView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                LayoutInflater inflater = LayoutInflater.from(MainActivity.this);
+                final View inputView = inflater.inflate(R.layout.dialog, null);
+
                 AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                builder.setMessage("AlertDialog").setPositiveButton("挨拶", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        Log.d("log",String.valueOf(i));
-                    }
-                });
-                builder.show();
+                builder.setMessage("AlertDialog");
+                builder.setView(inputView);
+
+
+
+
+//                builder.setPositiveButton("挨拶", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialogInterface, int i) {
+//                        Log.d("log",String.valueOf(i));
+//                    }
+//                });
+//                builder.setPositiveButton("Greeting", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialogInterface, int i) {
+//                        Log.d("log",String.valueOf(i));
+//                    }
+//                });
+                builder.create().show();
             }
         });
 
