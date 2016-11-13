891fbf853d38d3c1db14066ad390d25c9ff30f87
yamayosi <vanha.muist@gmail.com>
2016-11-14 00:26:46 +0900

    TextViewをタップでAlertDialog発生

--- app/src/main/java/com/zaki/hello/MainActivity.java
+++ app/src/main/java/com/zaki/hello/MainActivity.java
@@ -5,8 +5,14 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import static android.widget.Toast.LENGTH_SHORT;
 
 public class MainActivity extends Activity {
+
 
 
     @Override
@@ -14,14 +20,22 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setPositiveButton("挨拶", new DialogInterface.OnClickListener() {
+
+        TextView textView = (TextView)findViewById(R.id.text01);
+        textView.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                Log.d("log", String.valueOf(i));
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setMessage("AlertDialog").setPositiveButton("挨拶", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Log.d("log",String.valueOf(i));
+                    }
+                });
+                builder.show();
             }
         });
-        builder.show();
+
     }
 
 }
